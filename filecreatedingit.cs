8dc91ec82a49911ddeb0d99131ce582ab042dc54
pratapreddyg08 <59654706+pratapreddyg08@users.noreply.github.com>
2020-01-30 20:20:07 +0530

    Create filecreatedingit.cs
    A new file is created in Git Repository and it is sent to the system repository

---
+++ filecreatedingit.cs
@@ -0,0 +1,11 @@
+using System;
+console RocketLancher
+public class Filecreatedingit
+{
+public static void main()
+{
+Console.WriteLine("A File is Added in Git Repository");
+Console.WriteLine("File name is Filecreatedingit");
+Console.ReadLine();
+}
+}
